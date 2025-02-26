2ca11da63a10f2da261ea13e16d75ee1c27460eb
Tan Xin Jie <xjtan99@gmail.com>
2025-02-26 20:53:42 +0800

    Initial Commit

---
+++ pAI/pAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  pAIApp.swift
+//  pAI
+//
+//  Created by Tan Xin Jie on 26/2/25.
+//
+
+import SwiftUI
+
+@main
+struct pAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
